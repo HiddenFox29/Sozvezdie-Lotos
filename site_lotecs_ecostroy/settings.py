0810658a98895a395cacb2d10f08ef4f48418483
Alex <asiaimpex.kz@gmail.com>
2019-04-26 19:33:41 +0600

    Update DEBUG

--- site_lotecs_ecostroy/settings.py
+++ site_lotecs_ecostroy/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['lotex-ecostroy.herokuapp.com','127.0.0.1']
 
 
 # Application definition
