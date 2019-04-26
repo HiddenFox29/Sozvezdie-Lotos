e037f78f870a7ecb64afc56c60a96ab922bfdeb4
Alex <asiaimpex.kz@gmail.com>
2019-04-26 19:10:51 +0600

    Update DEBUG

--- site_lotecs_ecostroy/settings.py
+++ site_lotecs_ecostroy/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 # переводим debag в false чтобы не показвало ошибки разработки
 
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', False) )
 
 ALLOWED_HOSTS = ['*']
 
